822fd732f3c89a4bb50b027d0ea0bb0892b54446
Dalton Hart <DaltonVaren@gmail.com>
2018-11-29 10:49:24 -0800

    initial file structure and node setup

---
+++ server.js
@@ -0,0 +1,29 @@
+// require express and other modules
+const 
+    express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json())
+
+// serve static files from public folder
+app.use(express.static(__dirname + '/public'));
+
+/*
+ * HTML Endpoints
+ */
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+/*
+ * Response Endpoints
+ */
+
+
+//Run server and run on port 3000
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
